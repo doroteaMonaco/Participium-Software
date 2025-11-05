753423205f97da9d70e38802f4ff28f2b00e6171
Davide Colabella <davidecola16@gmail.com>
2025-11-05 17:00:01 +0100

    Swagger and docker fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,17 +1,24 @@
 import express from 'express';
 import cors from 'cors';
-import userRouter from './routes/userRouter';
+import authRouter from './routes/authRouter';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 const app = express();
+const swaggerDocument = YAML.load('../doc/OpenAPI_swagger.yml');
 
 app.use(cors());
 app.use(express.json());
-
-app.use('/api/users', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((err: any, _req: any, res: any, _next: any) => {
   console.error(err);
   res.status(500).json({ error: 'Internal Server Error' });
+});
+
+app.get('/', (_req, res) => {
+  res.send('Backend is running');
 });
 
 export default app;
