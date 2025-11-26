0063c6028148a14cadd7dd9f3ae5636333aa6f47
DOROTEA MONACO <dorotea.monaco@gmail.com>
2025-11-26 11:41:46 +0100

    fix error in reportDto

--- backend/src/models/dto/reportDto.ts
+++ backend/src/models/dto/reportDto.ts
@@ -6,7 +6,6 @@ export interface CreateReportDto {
   category: string;
   anonymous: boolean;
   photoKeys: string[]; // Keys for images stored in Redis
-  userId: number;
 }
 
 export interface ReportResponseDto {
